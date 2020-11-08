1cfa6cd4f2373b95da496f17c577a83d705a21ce
Raymond <shanjw@yingmi.cn>
2020-11-08 21:20:04 +0800

    Add redis

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import { Fetch_Posts } from "../modals/Post";
+import { redisClient, lPush } from "../redis";
+import logger from "../env/logger";
+
+export const preLoadPostsList = (key: string, data: Array<Fetch_Posts>): Promise<Array<Fetch_Posts> | null> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await lPush(key, data.map(val => JSON.stringify(val)));
+      redisClient.expire(key, 10000);
+      resolve() 
+    } catch (error) {
+      logger.error(error);
+      reject(error);
+    }
+  })
+}
